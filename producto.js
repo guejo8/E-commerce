d01ba5ab769a52843cc0afd317796786dd55a9b4
MariaDiezEsteve <diezesteve@outlook.es>
2023-02-13 08:40:39 +0100

    incluido producto descripción en el producto.js

--- producto.js
+++ producto.js
@@ -15,7 +15,7 @@ function changeCardContainerProducto(){
                     <a href="producto.html"><img src=${producto.foto} class="card-img-top" alt=${producto.name}></a>
                         <div class="card-body">
                         <h5 class="card-title">${producto.name}</h5>
-                        <p class="card-text">${producto.descripction}</p>
+                        <p class="card-text">${producto.description}</p>
                         <p class="card-text">${producto.precio} €</p>
                         <p class="cantidad">Cantidad &nbsp;&nbsp;<input id="cantidad${producto.idProd}" style="width:3rem" type="number" value="1" min="1"/></p>                   
                         <p href="#" class="btn3" producto=${producto.idProd}>Añadir al carrito</p>
